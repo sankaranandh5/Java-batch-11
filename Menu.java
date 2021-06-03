6654de65a70ca38b165d5b07a321297ac1da6152
sankaranandh5 <sankarsanandh@gmail.com>
2021-06-03 16:01:13 +0530

    Add files via upload

---
+++ Menu.java
@@ -0,0 +1,19 @@
+package com.san.entities;
+
+public class Menu {
+	public Item[] item;
+
+	public Menu(Item[] item) {
+		super();
+		this.item = item;
+	}
+
+	public Item[] getItem() {
+		return item;
+	}
+
+	public void setItem(Item[] item) {
+		this.item = item;
+	}
+
+	}
